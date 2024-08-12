d68f7f2c4c3430c394796c7d68c4bf5091a4e039
KATHIRVEL133 <velkathir259@gmail.com>
2024-08-12 13:24:20 +0530

    connected with the databases

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express(); 
+const dotenv = require('dotenv');
+dotenv.config();
 const PORT = 3000 || process.env.PORT;
+mongoose.connect(process.env.MONGO).then(()=>
+{
+    console.log('connected to MongoDB!');
+}).catch((err)=>{console.log(err);})
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
 });
