3a2c9a97342dc5c56e0a8c74179c9fc195616126
KATHIRVEL133 <velkathir259@gmail.com>
2024-08-13 09:39:58 +0530

    created test api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const PORT = 3000 || process.env.PORT;
+const userRouter = require('./routes/user.route.js')
 mongoose.connect(process.env.MONGO).then(()=>
 {
     console.log('connected to MongoDB!');
@@ -11,3 +12,4 @@ mongoose.connect(process.env.MONGO).then(()=>
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
 });
+app.use('/api/user',userRouter);
