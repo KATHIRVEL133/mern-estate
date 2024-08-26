0a1e171ca58152f01f50e116ec6007fb93e0f307
KATHIRVEL133 <velkathir259@gmail.com>
2024-08-26 16:05:59 +0530

    craete api get listing route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.models.js'
+import Listing from "../models/listing.model.js";
 export const test = (req,res) =>
 {
     res.json({
@@ -45,5 +46,24 @@ res.status(200).json("Deleted Successfully");
 catch(error)
 {
     next(error);
+}
+}
+export const getUserListings = async (req,res,next)=>
+{
+if(req.user.id===req.params.id)
+{
+try
+{
+const listings = await Listing.find({userRef:req.params.id});
+res.status(200).json(listings);
+}
+catch(error)
+{
+    next(error);
+}
+}
+else
+{
+    return next(errorHandler(401,'You can view only your own listing'));
 }
 }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router = express.Router();
-import {deleteUser, test, updateUser} from '../controllers/user.controller.js'
+import {deleteUser, getUserListings, test, updateUser} from '../controllers/user.controller.js'
 import { verifyUser } from '../utils/verifyUser.js';
-router.get('/test',test)
+router.get('/test',test);
 router.post('/update/:id',verifyUser,updateUser);
 router.delete('/delete/:id',verifyUser,deleteUser);
+router.get('/listings/:id',verifyUser,getUserListings);
 export default router
