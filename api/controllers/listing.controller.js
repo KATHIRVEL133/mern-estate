fe4a795a91bba9f8d6aa3b7f24b30ef826316220
KATHIRVEL133 <velkathir259@gmail.com>
2024-08-27 10:28:47 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req,res,next)=>
 {
@@ -13,3 +14,24 @@ export const createListing = async (req,res,next)=>
         next(error);
     }
 }
+export const updateListing = async (req,res,next)=>
+    {
+    const listing = await Listing.findById(req.params.id);
+    if(!listing)
+    {
+        return next(errorHandler(404,'list is not present here'));
+    }
+    if(req.user.id!==listing.userRef)
+    {
+        return next(errorHandler(401,'You can delete only your listing'));
+    }
+    try
+    {
+       const updatedListing =  await Listing.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        res.status(200).json(updatedListing);
+    }
+    catch(error)
+    {
+        next(error);
+    }
+    }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { createListing } from "../controllers/listing.controller.js";
+import { createListing,updateListing } from "../controllers/listing.controller.js";
 import { deleteListing } from "../controllers/user.controller.js";
 const router = express.Router();
 
 
 router.post('/create',verifyUser,createListing);
 router.delete('/delete/:id',verifyUser,deleteListing);
+router.post('/update/:id',verifyUser,updateListing);
 export default router;
