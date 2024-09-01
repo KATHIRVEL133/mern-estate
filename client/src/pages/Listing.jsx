2e06db109472126731f7ff3e7d265d4243ea1854
KATHIRVEL133 <velkathir259@gmail.com>
2024-09-01 19:02:47 +0530

    complete listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -5,12 +5,14 @@ import {Swiper,SwiperSlide} from 'swiper/react'
 import SwiperCore from 'swiper';
 import {Navigation} from 'swiper/modules';
 import 'swiper/css/bundle'
+import {FaBath, FaBed, FaChair, FaMapMarkerAlt, FaParking} from 'react-icons/fa'
 export default function Listing() {
 SwiperCore.use([Navigation]);
 const [listing,setListings] = useState(null);
 const [error,setError] = useState(false); 
 const [loading,setLoading] = useState(false);
 const params = useParams();
+console.log(listing);
 useEffect(()=>{
     const fetchData = async () =>
     {
@@ -19,6 +21,7 @@ useEffect(()=>{
         setError(false);
         const res = await fetch(`/api/listing/getListing/${params.listingId}`)
         const data = await res.json();
+        console.log(data);
         if(data.success===false)
         {
             setError(true);
@@ -30,12 +33,13 @@ useEffect(()=>{
     }
     catch(error)
     { 
+    setLoading(false);
      setError(true);
     }
  
-    }
+    };
     fetchData();
-},[params.listingId])
+},[params.listingId]);
   return (
     <main>
         {loading&&<p className="text-center text-2xl">
@@ -45,7 +49,7 @@ useEffect(()=>{
             Something went wrong
             </p>}
         
-           { listing&&!error&&!loading&&<div>
+           { listing&&!error&&!loading&&(<div>
            
             <Swiper navigation>
                 {
@@ -60,10 +64,59 @@ useEffect(()=>{
                     )
                 }
             </Swiper>
+            <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+           <p className='text-2xl font-semibold'>
+             {listing.name} - ${' '}
+             {listing.offer
+               ? listing.discountPrice.toLocaleString('en-US')
+               : listing.regularPrice.toLocaleString('en-US')}
+             {listing.type === 'rent' && ' / month'}
+           </p>
+           <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
+             <FaMapMarkerAlt className='text-green-700' />
+             {listing.address}
+           </p>
+           <div className="flex gap-3">
+            <p className="bg-red-900 w-full max-w-[200px] text-center text-white p-1 rounded-md">
+                {listing.type=='rent'?'For Rent':'For sale'}
+            </p>
+            {
+            listing.offer?
+            <p  className="bg-green-900 w-full max-w-[200px] text-center text-white p-1 rounded-md">
+             ${+listing.regularPrice-+listing.discountPrice}
+            </p>:''
+            }
            </div>
-            
+           <p>
+           <span className="font-semibold">
+            Description-
+           </span>
+            {listing.description}
+           </p>
+           <ul className="flex flex-wrap gap-4 sm:gap-6 text-green-900 font-semibold text-sm text-center">
+            <li className="flex gap-1 items-center whitespace-nowrap">
+                <FaBed className="text-lg"/>
+                {listing.bedrooms>1?listing.bedrooms+' beds':listing.bedrooms+' bed'}
+            </li>
+            <li className="flex gap-1 items-center">
+                <FaBath className="text-lg"/>
+                {listing.bathrooms>1?listing.bathrooms+' baths':listing.bathrooms+' bath'}
+            </li>
+            <li className="flex gap-1 items-center">
+                <FaParking className="text-lg"/>
+                {listing.parking?'parking available':'No Parking'}
+            </li>
+            <li className="flex gap-1 items-center">
+                <FaChair className="text-lg"/>
+                {listing.furnished?'Furnished':'Not Furnished'}
+            </li>
+           </ul>
+           </div>
+           </div>
+         
+           
+           )
         }
-        
     </main>
   )
 }
