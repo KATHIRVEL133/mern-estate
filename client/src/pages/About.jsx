51d90afde48e35365e4cc06db6650f23cf713496
KATHIRVEL133 <velkathir259@gmail.com>
2024-09-05 09:50:01 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,8 +3,19 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='max-w-6xl mx-auto flex flex-col gap-3 my-10 font-serif'>
+     <h1 className='text-6xl font-bold text-slate-600 '>
+      About Sundar Estate
+     </h1>
+     <p className='text-2xl'>
+     At <span className='text-slate-600'>Sundar Estate</span>, we believe that finding the perfect home or investment property should be an enjoyable and stress-free experience. With over <span className='text-red-700'>10</span> years of experience in the real estate industry, we specialize in helping clients buy, sell, and rent residential and commercial properties throughout ,<span className='text-red-700'>Tamil Nadu.</span>
+     </p>
+     <p  className='text-2xl'>
+     Our team is comprised of passionate, dedicated agents who are experts in the local market, providing personalized service and up-to-date market insights to help you make informed decisions. Whether youre a first-time homebuyer, a seasoned investor, or looking for the perfect rental, we are committed to guiding you through every step of the process with transparency, integrity, and professionalism.
+     </p>
+     <p  className='text-2xl'>
+     At <span className='text-slate-600'>Sundar Estate</span>, we understand that real estate is more than just a transaction; it’s about building long-term relationships and making your property dreams a reality. Contact us today, and let’s find your perfect place together.
+     </p>
     </div>
   )
 }
